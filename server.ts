2b403f9b568ae7d7cd5c8b43bff5b83e8ca390b9
cdan-221 <chiaremari@gmail.com>
2024-02-07 00:27:40 -0500

    put skid/:id api works

--- server.ts
+++ server.ts
@@ -1,10 +1,9 @@
-//const express = require('express');
-//const cors = require('cors');
 import express from 'express';
 // import cors from 'cors';
 import { Request, Response } from 'express';
 
 const app = express();
+app.use(express.json()); // Enable JSON body parsing
 // used a proxy server in the angular client app instead of cors
 /* 
 const corsOptions = {
@@ -37,6 +36,25 @@ app.get('/api/skids', (req: Request, res: Response) => {
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from your Digital Forms server! See my API documentation at ');
+});
+
+app.put('/api/skids/:id', (req: Request, res: Response) => {
+  // Extract the id from the request parameters
+  let id = Number(req.params.id) - 1; // subtract 1 to convert from 1-based to 0-based index
+
+  // Extract the new data for the skid from the request body
+  let newSkidData = req.body;
+
+  // assign the newSkidqData to the skid with the id
+  skids[id] = newSkidData;
+  
+  console.log('Updated skid', id, 'with', newSkidData);
+  // todo should this status be 204, no content?
+  res.status(200).send({message: 'OK'});
+
+  // todo handle a case where the skid number is out of range
+  // res.status(404).send({message: 'Skid not found'});
+  
 });
 
 app.listen(port, () => {
